c9b6eda0c89a33e0bd59c7e5f0b3ba01214ebc33
thybag <thybag@gmail.com>
2014-01-30 20:10:50 +0000

    Pretty up demo site
    * Use bootstrap theme (from CDN)
    * Incorporate bootstrap elements
    * Improve text & information
    * Show off more features

--- test/index.php
+++ test/index.php
@@ -4,30 +4,46 @@ $title = 'PJAX-Standalone';
 $contents = '
 	<h1>PJAX-Standalone</h1>
 	
-	<p>Pjax-Standalone is a standalone implimention of defunkt\'s original
-	<a href=\'https://github.com/defunkt/jquery-pjax\'>JQuery-PJax</a> plugin which has no dependencies and thus can be dropped in to, and used, on any webpage
-	you wish, regardless of the JavaScript framework employed (or even if none is used at all).
+	<p>
+		Pjax-Standalone is a standalone implementation of defunkt\'s original
+		<a href=\'https://github.com/defunkt/jquery-pjax\'>jQuery-PJAX</a> plugin. With no external dependencies, PJAX-Standalone can 
+		be dropped into and used on any website, regardless of the JavaScript frameworks in use.
+	</p>
+	<h3>About</h3>
+	<p>
+		PJAX Standalone is easy to set up and get working with both new and existing websites. Adding the PJAX-Standalone 
+		JavaScript to the head of your website and calling <code>pjax.connect("id_of_container");</code> is all thats needed
+		to get it working.
 	</p>
 	<p>
-	Like its counterpart, PJAX-Standalone is designed to be incredibly easy to setup and add in
-	to existing web pages. In many sites nothing more will be needed than inclusion of the script in to the page
-	and a single call to "pjax.connect(\'my_content_div\');"
-
-
+		By default PJAX-Standalone uses it\'s "smartload" feature in order to work with existing with pages with no server side support for PJAX. If 
+		your server side code is configured to work with PJAX (thus only returning the content needed for rendering) this feature can be 
+		disabled for even more performance gains.</p>
+	<p>
+		<strong>Is PJAX-Standlone worth having on my site?</strong><br/>
+		PJAX provides the most benefit if your website design comprises primarily of one main content area, surrounded by a 
+		wrapper (header, footer, aside) that rarely change. Since PJAX only ever has to reload the main container area\'s content, assets such as CSS, JavaScript, images and others will only be loaded once.
 	</p>
 
+	<p>
+		<strong>How customizable is PJAX-Standalone</strong><br/>
+		PJAX-Standalone fires a number of events, which can be used to extend its native functionality. Additionally
+		a number of additional options can be specified in the connect method, in order to further configure how PJAX-Standalone functions. 
+		Documentation on the events and options available in PJAX can be seen in the <a href="https://github.com/thybag/PJAX-Standalone/t">github readme</a>.
+	</p>
+	<a name="what"></a>
 	<h3>What is PJAX</h3>
 	<p>
-	PJAX (Push-state AJAX) for the most part is a speed optimisation. 
-	It allows users to navigate your website without ever having to reload 
-	the entire page (or wait for the browser to redraw the main layout)
-
-	while still preserving the page titles, permalinks and full back button functionality. Additionally, if 
-	a browser does not support the PJAX feature set, the links will simply work as normal, allowing
-	it to degrade gracefully without ever impacting the functionality of a website.
+		PJAX (Push-state AJAX) is a performance optimization, allowing users to browse websites without the overhead of 
+		reloading assets such as CSS & images each time a web page is loaded.
 	</p>
-
-	
+	<p>
+		PJAX works by using AJAX to "load" pages on behalf of the user, then dynamically swapping the contents of
+		the "container" for the newly loaded content from the link the user clicked. Unlike older AJAX solution\'s PJAX
+		fully preserves permalinks and back button functionality. Using progressive enhancement, PJAX will 
+		seamlessly fall back to loading pages in the normal way in older browsers, while newer browsers can make use
+		of the improved performance.
+	</p>	
 ';
 
 if(isset($_SERVER['HTTP_X_PJAX']) && $_SERVER['HTTP_X_PJAX'] == 'true'){
--- test/page1.php
+++ test/page1.php
@@ -1,11 +1,15 @@
 <?php
 
-$title = 'Page 1: Hello Tester';
+$title = 'Demo 1: Is it working?';
 $contents = '
-	<h1>Page 1 - Actual time.</h1>
+	<h1>Demo 1 - Actual time.</h1>
 	<p>The real time is: <strong>'.date('l jS \of F Y h:i:s A').'</strong>.</p>
-	<p>If the above time does not match that of the "Loaded at" then this page was requested via PJAX.</p>
-	<p>Currently this page is simply returning the full HTML version of itself rather than PJAX ready content, although smartLoad fixes this seamlessly.</p>
+	<p>If the above time does not match that of the "Loaded at" (section on the right) then this page was successfully requested via PJAX.</p>
+	<p>Currently this page is simply returning the full HTML version of itself rather than PJAX ready content, meaning smartLoad is being used to seamlessly extract only the required content.</p>
+
+	<h3>Invoke</h3>
+	<p>The below button uses the <code>pjax.invoke()</code> method to load the home page (via PJAX if it is supported).</p>
+	<button onclick="pjax.invoke(\'index.php\', \'content\');" class="btn">Try me</button>
 ';
 
 //Page 1 returns the full HTML, not the PJAX content and is then correct by the smartLoad feature.
